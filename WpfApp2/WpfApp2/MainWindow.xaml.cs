9456f168d4b1acf01dea2e408253b57e9881d1b5
TAKIO-PC\takio <taka21centry@gmail.com>
2017-10-03 00:54:38 +0900

    クリックイベント

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private int count = 0;
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            btn.Content = string.Format("{0}回", ++count);
+        }
+    }
+}
